8f1317657644b7bc6486fec2f217ef8b551fcfdf
Endijs Ogriņš <ogrins92@gmail.com>
2023-07-02 12:30:12 +0300

    Updated

--- projects/00 - NameGenerator/main.cpp
+++ projects/00 - NameGenerator/main.cpp
@@ -1,6 +1,17 @@
+// Random name generator C++ program
 #include <iostream>
 
 int main()
-{
+{   
+    // Ask for the user for y or n random name generation
+    std::cout << "Would you like to generate a random name? (y/n): ";
+    char answer;
+    std::cin >> answer;
+    // If the user answers y, generate a random name
+    if (answer == 'y')
+    {
+        std::cout << "Generating a random name...\n";
+    }
+
     std::cout << "Test1!\n";
 }
